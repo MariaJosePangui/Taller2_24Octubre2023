a7a6f246a162e52e0492e53ac42f4aa94ffe8313
cotep <m.pangui01@ufromail.cl>
2023-10-25 23:07:15 -0300

    Subo readme

---
+++ README.md
@@ -0,0 +1,7 @@
+Taller 2 (26 de Octubre 2023)
+
+Maria Jose Pangui y Esteban Seron
+
+20884018520 y 20080692120
+
+ICI-I
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.taller2_261023 {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.taller2_261023 to javafx.fxml;
+    exports com.example.taller2_261023;
+}
