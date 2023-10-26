f739a1a2011dc865a5b13f1a7dff9880b14512ea
cotep <m.pangui01@ufromail.cl>
2023-10-26 08:47:27 -0300

    primer commit

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.taller2_261023 {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                            
-    opens com.example.taller2_261023 to javafx.fxml;
-    exports com.example.taller2_261023;
-}
